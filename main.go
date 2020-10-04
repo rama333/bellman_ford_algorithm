59d35eef7a3a08ac14664a1b7e94327118cbb626
ramilramilev <barsictcp@gmail.com>
2020-10-04 13:20:54 +0300

    ok

--- main.go
+++ main.go
@@ -3,17 +3,17 @@ package main
 import "fmt"
 
 type Graph struct {
-	a int
-	b int
+	a    int
+	b    int
 	edge int
 }
 
-func main()  {
+func main() {
 
-	inf := 1<<31
+	inf := 1 << 31
 	v := 0
 
-	graphs:= []Graph{}
+	graphs := []Graph{}
 
 	graphs = append(graphs, Graph{a: 0, b: 1, edge: 7})
 	graphs = append(graphs, Graph{a: 1, b: 2, edge: -5})
@@ -25,30 +25,29 @@ func main()  {
 	m := len(graphs)
 	n := len(graphs)
 
-
 	for i := 0; i < n-1; i++ {
 		d = append(d, inf)
 	}
 
 	d[v] = 0
 
-
-	for i:=0; i< n-1; i++{
+	for i := 0; i < n-1; i++ {
 		for j := 0; j < m; j++ {
-			if d[graphs[j].a] < inf{
-				d[graphs[j].b] = min(d[graphs[j].b], d[graphs[j].a] + graphs[j].edge )
-				fmt.Println(i,j , d[graphs[j].b], d[graphs[j].a] + graphs[j].edge )
+			if d[graphs[j].a] < inf {
+				d[graphs[j].b] = min(d[graphs[j].b], d[graphs[j].a]+graphs[j].edge)
+				fmt.Println(i, j, d[graphs[j].b], d[graphs[j].a]+graphs[j].edge)
 			}
 
 		}
 	}
 
-	fmt.Println(d)
+	fmt.Println(d) //return [0 7 2 6]
+
 }
 
-func min(a int, b int)  (int){
+func min(a int, b int) int {
 
-	if a<b{
+	if a < b {
 		return a
 	}
 	return b
